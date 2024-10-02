8b483548cd151e86d0dcb0cee0becece7e6ea384
Ahmed nafees m <149936709+Ahmednaf03@users.noreply.github.com>
2024-10-02 13:33:17 +0530

    nav-finished-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',  // Includes all files in the `pages` directory for Next.js
+    './components/**/*.{js,ts,jsx,tsx}',  // Includes files in the `components` directory
+    './app/**/*.{js,ts,jsx,tsx}',  // Includes files in the `app` directory if using the App Router (Next.js 13+)
   ],
   theme: {
     extend: {
+      fontFamily: {
+        generalsans: ['General Sans', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: '#000',
+          100: '#010103',
+          200: '#0E0E10',
+          300: '#1C1C21',
+          500: '#3A3A49',
+          600: '#1A1A1A',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          800: '#E4E4E6',
+          700: '#D6D9E9',
+          600: '#AFB0B6',
+          500: '#62646C',
+        },
+      },
+      backgroundImage: {
+        terminal: "url('/assets/terminal.png')",
       },
     },
   },
