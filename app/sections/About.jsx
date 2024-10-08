38dd6e35c9c43b267b6d703f873189f911560a24
Ahmed nafees m <149936709+Ahmednaf03@users.noreply.github.com>
2024-10-09 02:04:23 +0530

    About section is done, weeeee

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -1,7 +1,18 @@
-
+"use client"
 import React from 'react'
-import { Globe } from "react-globe.gl"
+import Globe from 'react-globe.gl';
+import Button from '../components/Button';
+import { useState } from 'react';
 const About = () => {
+    const[hasCopied,setHasCopied]=useState(false);
+    const handleCopy = () => {
+        navigator.clipboard.writeText("ahamednafees777@gamail.com");
+        setHasCopied(true);
+    
+        setTimeout(() => {
+          setHasCopied(false);
+        }, 2000);
+      };
   return (
     <section className="c-space my-20">
         <div className="grid xl:grid-cols-3
@@ -34,7 +45,49 @@ const About = () => {
                 <div className="grid-container">
                     <div className="rounded-3xl w-full sm:h-[326px]
                     h-fit flex justify-center items-center">
-                        {/* <Globe/> */}
+                <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[{ lat: 13, lng: 80, text: 'i am here', color: 'white', size: 15 }]}
+              />
+
+                    </div>
+                    <div>
+                        <p className="grid-headtext"> I work remotely across timezones</p>
+                        <p className="grid-subtext">I'm based in india with Remote work availability.</p>
+                        <Button name="contact me" isBeam containerClass="w-full mt-10" />
+                    </div>
+                </div>
+             </div>
+             <div className="xl:col-span-2 xl:row-span-2">
+                <div className="grid-container">
+                    <img src="../assets/grid3.png" alt="grid-3" 
+                    className="w-full sm:h-[266px] h-fit object-contain" />
+                    <div>
+                        <p className="grid-headtext"> My passion for coding</p>
+                        <p className="grid-subtext">I love solving problems and creating things through code
+                            and I'm always looking for new challenges.
+                        </p>
+                    </div>
+                </div>
+             </div>
+             <div className="xl:col-span-1 xl:row-span-2">
+                <div className="grid-container">
+                    <img src="../assets/grid4.png" alt="grid-4" 
+                    className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
+                    <div className="space-y-2">
+                        <p className="grid-subtext text-center">Contact me</p>
+                        <div 
+                        className="copy-container" onClick={handleCopy} >
+                            <img src={hasCopied ? "../assets/tick.svg" : "../assets/copy.svg"} alt="copy" />
+                            <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">ahamednafees777@gmail.com</p>
+                        </div>
                     </div>
                 </div>
              </div>
@@ -43,4 +96,4 @@ const About = () => {
   )
 }
 
-export default About
+export default About;
