8f51d5195c3b2556b7a82c8ce7d0f525bd65ec5d
Ahmed nafees m <149936709+Ahmednaf03@users.noreply.github.com>
2024-11-15 19:37:50 +0530

    more performance optimization

--- app/sections/Experience.jsx
+++ app/sections/Experience.jsx
@@ -5,10 +5,12 @@ import { Canvas } from '@react-three/fiber'
 import { workExperiences } from '../constants'
 import { OrbitControls } from '@react-three/drei'
 import CanvasLoader from '../components/CanvasLoader'
-import Developer from '../components/Developer'
+// import Developer from '../components/Developer'
 import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
-
+const Developer = dynamic(() => import('../components/Developer'), { 
+  ssr: false
+})
 const PhExperience = dynamic(() => import('../sections/PhExperience'), { 
   ssr: false
 })
